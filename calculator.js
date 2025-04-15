3d886fe0d25f3eca61157c6c7d3234f390bbcaff
João Pedro <jpsgamerz@gmail.com>
2025-04-15 10:19:45 -0300

    ✨ Funções de some e raiz quadrada adicionadas
    Funções testadas e atualizadas

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
-
+function sum(v1, v2) {
+    return v1 + v2;
+}
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {}
 
@@ -11,6 +12,9 @@ function div(v1, v2) {}
 function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    return Math.sqrt(v1);
+}
 
 console.log(sum(v1, v2));
+console.log(square(v1));
