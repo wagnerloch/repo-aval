b8536e831ffa8379dbb494946f18687ebd527c66
silvio-cast <silvio.importante@gmail.com>
2025-04-15 21:05:31 -0300

    :sparkles: adicionar função raiz quadrada

--- calculator.js
+++ calculator.js
@@ -2,9 +2,7 @@
 function sum(v1, v2) {}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {
-    return v1 - v2;
-}
+function sub(v1, v2) {}
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
@@ -13,6 +11,8 @@ function div(v1, v2) {}
 function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    return Math.sqrt(v1);
+}
 
 console.log(sum(v1, v2));
