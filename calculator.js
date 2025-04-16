f9c9062e0abdf4ce520a7ed113fd3ba1dcf34b42
silvio-cast <silvio.importante@gmail.com>
2025-04-15 21:27:19 -0300

    :pencil2: Corregir erros de digitação

--- calculator.js
+++ calculator.js
@@ -2,7 +2,9 @@
 function sum(v1, v2) {}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {
+    return v1 - v2;
+}
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
