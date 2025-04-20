37d9be3879f746fa9fbbd4175cd0f1415ad87189
Henrique Laroque <henriquelaroque@outlook.com>
2025-04-20 16:16:43 -0300

    ✨ Nova função de soma adicionada

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
+function sum(v1, v2) {
+    return v1 + v2
+}
 
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {}
