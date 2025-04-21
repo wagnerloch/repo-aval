11af3ab61e739b28d658221252a3861903e5d94a
Felipe Ponciano <felipeponciano7@gmail.com>
2025-04-20 22:50:59 -0300

    ✨ Cria a funcionalidades raiz e multiplicação
    Cria as funcionalidades raiz e multiplicação

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+const v1 = 10;
+const v2 = 5;
+
 // retorna o valor da soma entre os dois parâmetros
 function sum(v1, v2) {}
 
@@ -8,9 +11,15 @@ function sub(v1, v2) {}
 function div(v1, v2) {}
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    return v1 * v2;
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+      return Math.sqrt(v1);
+}
+    
+
 
 console.log(sum(v1, v2));
