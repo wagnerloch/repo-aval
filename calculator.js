869df6bd8f3c414a9e67f4ddafc334c3ac8bec5b
Mateus Fagundes <mateus.fagundes@betalent.tech>
2025-04-22 19:35:58 -0300

    ✨ add function mult

--- calculator.js
+++ calculator.js
@@ -8,7 +8,9 @@ function sub(v1, v2) {}
 function div(v1, v2) {}
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    return v1 * v2;
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
