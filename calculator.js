61b02b91210620ef749223ed0308c152e1628171
Vinicius Dobke <vinicius.d.gaspar@gmail.com>
2025-04-22 14:28:10 -0300

    ✨ Adiocina um menu para a calculadora

--- calculator.js
+++ calculator.js
@@ -1,3 +1,60 @@
+const prompt = require("prompt-sync")();
+
+while (true) {
+    console.log("1. Soma");
+    console.log("2. Subtração");
+    console.log("3. Multiplicação");
+    console.log("4. Divisão");
+    console.log("5. Radiciação");
+    console.log("0. Sair")
+    console.log();
+    const opcao = Number(prompt("Qual operação você deseja realizar? "));
+
+    if (opcao == 0) {
+        break;
+    }
+
+    switch (opcao) {
+        default:
+            console.log("Opção Inválida");
+            console.log();
+            break;
+
+        case 1:
+            console.log("Informe os números que você deseja realizar a soma: ");
+            const sum1 = Number(prompt("Primeiro número: "));
+            const sum2 = Number(prompt("Segundo número: "));
+            console.log(`O resultado é: ${sum(sum1, sum2)}`);
+            break;
+
+        case 2:
+            console.log("Informe o minuendo e o subtraendo, respectivamente, para realizar a subtração: ");
+            const sub1 = Number(prompt("Minuendo: "));
+            const sub2 = Number(prompt("Subtraendo: "));
+            console.log(`O resultado é: ${sub(sub1, sub2)}`);
+            break;
+
+        case 3:
+            console.log("Informe o multiplicando e o multiplicador, respectivamente, para realizar a multiplicação: ");
+            const mult1 = Number(prompt("Multiplicando: "));
+            const mult2 = Number(prompt("Multiplicador: "));
+            console.log(`O resultado é: ${mult(mult1, mult2)}`);
+            break;
+
+        case 4:
+            console.log("Informe o dividendo e o divisor, respectivamente, para realizar a divisão: ");
+            const div1 = Number(prompt("Dividendo: "));
+            const div2 = Number(prompt("Divisor: "));
+            console.log(`O resultado é: ${div(div1, div2)}`);
+            break;
+
+        case 5:
+            const root = Number(prompt("Informe o número do qual você deseja obter a raiz quadrada: "));
+            console.log(`O resultado é: ${square(root)}`);
+            break;
+    }
+}
+
 // retorna o valor da soma entre os dois parâmetros
 function sum(v1, v2) {
     return v1 + v2;
@@ -16,5 +73,3 @@ function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
-
-console.log(sum(v1, v2));
