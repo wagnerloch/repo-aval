40e5ce7cafb718ac4fc1c3db420b179c1b538586
Miguel Vencato <miguelvencato.cupim@gmail.com>
2025-04-23 00:06:02 -0300

    Add:Operação de divisão

--- calculator.js
+++ calculator.js
@@ -5,7 +5,9 @@ function sum(v1, v2) {}
 function sub(v1, v2) {}
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) {
+    divisao = v1 / v2
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {}
