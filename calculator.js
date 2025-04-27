ae7ed6871237d7ce0bdaaeafa4108fb47a608509
luisarabassa <luisarabassads@gmail.com>
2025-04-27 17:10:45 -0300

    ✨ Add feature mult
    Add feature multiplication

--- calculator.js
+++ calculator.js
@@ -25,7 +25,12 @@ function div(v1, v2) {
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    const v1 = Number(prompt("Primeiro valor: "));
+    const v2 = Number(prompt("Segundo valor: "));
+
+    return v1 * v2;
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
