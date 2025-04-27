3b8efe2d1dae17d74f9178301720a9a0e8c8a3b6
luisarabassa <luisarabassads@gmail.com>
2025-04-27 17:33:36 -0300

    🩹 Update calculator general functioning
    Made an if else function to determinate the operation

--- calculator.js
+++ calculator.js
@@ -1,37 +1,43 @@
 const prompt = require("prompt-sync")()
 
+console.log("Calculadora 🧮");
+const v1 = Number(prompt("Digite o primeiro valor: "))
+const op = prompt("Escolha a operação (+, -, *, /): ")
+const v2 = Number(prompt("Digite o segundo valor: "))
+
 // retorna o valor da soma entre os dois parâmetros
 function sum(v1, v2) {
-    const v1 = prompt(Number("Primeiro valor: "))
-    const v2 = prompt(Number("Segundo valor: "))
-
     return v1 + v2;
 }
 
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {
-    const v1 = Number(prompt("Primeiro valor: "));
-    const v2 = Number(prompt("Segundo valor: "));
-
     return v1 - v2;
 }
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {
-    const v1 = Number(prompt("Primeiro valor: "));
-    const v2 = Number(prompt("Segundo valor: "));
-
     return v1 / v2;
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {
-    const v1 = Number(prompt("Primeiro valor: "));
-    const v2 = Number(prompt("Segundo valor: "));
-
     return v1 * v2;
 }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
+if (op == "+") {
+    console.log(`${v1} + ${v2} = ${sum(v1, v2)}`)
+} else if (op == "-") {
+    console.log(`${v1} - ${v2} = ${sub(v1, v2)}`)
+} else if (op == "*") {
+    console.log(`${v1} * ${v2} = ${mult(v1, v2)}`)
+} else if (op == "/") {
+    if (v2 == 0) {
+        console.log("Divisão por zero não é permitida.")
+    } else{
+        console.log(`${v1} / ${v2} = ${div(v1, v2)}`)
+    }
+}
