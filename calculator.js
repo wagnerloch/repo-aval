1cd222066237d5463dcf3e8155c9c7af69e99ce5
Vitor Manoel <vitormanoelmb02@gmail.com>
2025-04-28 20:04:05 -0300

    ✨ Add multiplication function

--- calculator.js
+++ calculator.js
@@ -17,7 +17,9 @@ function div(v1, v2) {
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    return v1 * v2;
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
