d61b5b5b9541fb81f505b94c1ccd0640427503d0
Nugunus <ppedrornunes@gmail.com>
2025-04-28 18:07:49 -0300

    ✨ Raiza quadrada e ajustes (#6)
    adição da função raiz quadrada e retirada das intruções

--- calculator.js
+++ calculator.js
@@ -1,29 +1,30 @@
-const v1 = Number(prompt("Primeiro Numero: "))
-const v2 = Number(prompt("Sengundo Numero:"))
+console.log(sum(v1, v2));
+console.log(square(v1));
+=======
+const v1 = Number(prompt("Primeiro Numero: "));
+const v2 = Number(prompt("Sengundo Numero:"));
 
 function sum(v1, v2) {
    return(v1 + v2);
 }
 
-// retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {
     return (v1 - v2);
 }
 
-// retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {
     return(v1 / v2);
 }
 
-// retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {
     return(v1 * v2);
 }
-// retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
 
+function square(v1) {
+    return Math.sqrt(v1);
+}
 console.log(sum(v1, v2));
 console.log(sub(v1, v2));
 console.log(div(v1, v2));
 console.log(mult(v1, v2));
-
+console.log(square(v1));
