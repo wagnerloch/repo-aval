9c155e2c5ec59b2df967ded1a5f25a59d4a11e50
Enzo Sabbado <enzosabbado@gmail.com>
2025-04-28 13:46:49 -0300

    :recycle: Refatora função de divisão para melhorar leitura

--- calculator.js
+++ calculator.js
@@ -5,15 +5,12 @@ function sum(v1, v2) {}
 function sub(v1, v2) {}
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
-
-function dividir(a, b) {
-    if (b === 0) {
-      throw new Error("Divisão por zero não é permitida.");
-    }
-    return a / b;
+function div(v1, v2) {
+  if (v2 === 0) {
+    throw new Error("Divisão por zero não é permitida.");
   }
-  
+  return v1 / v2;
+} 
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {}
