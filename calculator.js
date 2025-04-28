a11cfd27dd0363b5d3c7fd81a329266ff20e3b6e
Anthony Escobar <anthonyescobar7@gmail.com>
2025-04-28 19:42:27 -0300

    🚩 Função de multiplicação implementada

--- calculator.js
+++ calculator.js
@@ -10,9 +10,12 @@ function div(v1, v2) {
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {return v1 * v2;}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
+
+console.log(sum(v1, v2));
 console.log(div(v1, v2));
+console.log(mult(v1, v2));
