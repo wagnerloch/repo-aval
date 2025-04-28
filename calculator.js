0bd214f48e9d3278a49b355d3022dcfd29259925
Robinson Abraham <abraham.robinson@outlook.com>
2025-04-28 20:56:57 -0300

    🚩 Função de raiz quadrada implementada

--- calculator.js
+++ calculator.js
@@ -15,10 +15,11 @@ function div(v1, v2) {
 function mult(v1, v2) {return v1 * v2;}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {return Math.sqrt(v1);}
 
 
 console.log(sum(v1, v2));
 console.log(sub(v1, v2));
 console.log(div(v1, v2));
 console.log(mult(v1, v2));
+console.log(square(v1));
