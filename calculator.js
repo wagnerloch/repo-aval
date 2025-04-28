ba83ebfc287068ae83c0700955ab94e2fece2c8a
Luana Amaral <luanasoaresdoamaral4@gmail.com>
2025-04-28 12:10:03 -0300

    sub adicionada

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 import prompt from 'prompt-sync';
 import sum from './sum.js';
 import div from './div.js';    
-import mul from './mul.js';
+import mult from './mul.js';
 import square from './square.js';
+import sub from './sub.js';
 
 const input = prompt();
 console.log('Calculadora');
@@ -34,10 +35,3 @@ switch(option) {
     default:
         console.log('Operação inválida');
 }
-
-// retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
-
-// retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
-
