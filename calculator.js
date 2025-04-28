99e146e0daf0b1432f4db93f17fa8d07ededb52e
juan nunes <juanrosanuness@gmail.com>
2025-04-28 17:39:20 -0300

    :sparkles: add sub function

--- calculator.js
+++ calculator.js
@@ -4,15 +4,17 @@ function sum(v1, v2) {
 }
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {
+    return v1 - v2;
+}
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) { }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) { }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) { }
 
 console.log(sum(v1, v2));
