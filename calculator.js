c1dc8863a1091978e2f8235c2ae2ecb30fb8818f
Vinicius Dobke <vinicius.d.gaspar@gmail.com>
2025-04-28 14:57:04 -0300

    🎨 Elimina blocos desnecessários e/ou redundantes

--- calculator.js
+++ calculator.js
@@ -1,10 +1,3 @@
-
-// retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
-
-// retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
-
 const prompt = require("prompt-sync")();
 
 while (true) {
@@ -76,28 +69,16 @@ function sum(v1, v2) {
 function sub(v1, v2) {
     return v1 - v2;
 }
+
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {
     return v1 / v2;
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
-
-// calculadora.js
-
-function multiplicar(a, b) {
-    return a * b;
-  }
-  
-  // Exporta a função, se você for usar em outro arquivo
-  module.exports = { multiplicar };
-  
-
-// retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
-
-console.log(sum(v1, v2));
+function mult(v1, v2) {
+    return v1 * v2;
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {
