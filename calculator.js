a17bb7317e59c095629108987da8a0f06516f4dd
Jquinzen <juanquinzen05@gmail.com>
2025-04-28 23:22:13 -0300

    :bug: correção da main em calculator.js

--- calculator.js
+++ calculator.js
@@ -1,16 +1,11 @@
-class Calculator {
- soma(a, b) {
-      return a + b;
-  }
+// retorna o valor da soma entre os dois parâmetros
+function sum(v1, v2) {}
 
-  subtração(a, b) {
-      return a - b;
-  }
+// retorna o valor da subtração entre os dois parâmetros
+function sub(v1, v2) {}
 
-  multiplicação(a, b) {
-      return a * b;
-  }
-}
+// retorna o valor da divisão entre os dois parâmetros
+function div(v1, v2) {}
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {}
@@ -18,11 +13,4 @@ function mult(v1, v2) {}
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
-module.exports = Calculator;
-
-
-// Testes 
-const calc = new Calculator();
-console.log("Soma: ", calc.soma(10, 5));
-console.log("Subtração: ", calc.subtração(10, 5));
-console.log("Multiplicação: ", calc.multiplicação(10, 5));
+console.log(sum(v1, v2));
