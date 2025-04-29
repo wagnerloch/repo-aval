7fa72c9e42ea5e16a001c35d6d010c46eb653193
Henrique F. da Silveira <henryfsilveira@MacBook-Air-de-Henrique.local>
2025-04-28 22:35:41 -0300

    :sparkles: add function multiplicacao

--- calculator.js
+++ calculator.js
@@ -29,7 +29,15 @@ function div(v1, v2) {
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult() {
+
+    let num1 = parseFloat(prompt("Digite o primeiro número:"));
+    let num2 = parseFloat(prompt("Digite o segundo número:"));
+    
+    let resultado = num1 * num2;
+    console.log(`Multiplicação entre ${num1} e ${num2} = ${resultado}`);
+}
+
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
