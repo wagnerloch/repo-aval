03e57b3d6c02fb31df7e97f944112db5889cf577
rafael neuenfeldt <rafael.neuenfeldt03@outlook.com>
2025-04-28 23:36:51 -0300

    ✨ adicionada a função de multiplicação

--- calculator.js
+++ calculator.js
@@ -2,19 +2,17 @@
 function sum(v1, v2) {
     return v1 + v2;
 }
-
-
-
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {
     return v1 - v2;
   }
-
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    return v1 * v2 
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {
@@ -26,3 +24,6 @@ function square(v1) {
 
 
 console.log(sum(v1, v2));
+console.log(sub(v1, v2));
+console.log(mult(v1, v2));
+console.log(square(v1));
