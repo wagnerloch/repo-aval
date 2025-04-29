20b11d8c34481b20b87a4271bb8bca8e40411b02
KarolineKS <kkohlsstangherlin@gmail.com>
2025-04-28 23:36:43 -0300

    :recycle: improved div function

--- calculator.js
+++ calculator.js
@@ -16,15 +16,14 @@ function sub() {
   console.log(`Subtração entre ${num1} e ${num2} = ${resultado}`);
 }
 
-// retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {
-  let num1 = parseFloat(prompt("Digite o primeiro número:"));
-  let num2 = parseFloat(prompt("Digite o segundo número:"));
+function div() {
+  const num1 = parseFloat(prompt("Digite o primeiro número: "));
+  const num2 = parseFloat(prompt("Digite o segundo número: "));
 
   if (v2 === 0) {
     return "Divisão por zero não é permitida";
   } else {
-    let resultado = num1 / num2;
+    const resultado = num1 / num2;
     console.log(`Divisão entre ${num1} e ${num2} = ${resultado}`);
   }
 }
