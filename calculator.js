0da58665b7f8a4f7a93966deccf803a900c5537c
rafael neuenfeldt <rafael.neuenfeldt03@outlook.com>
2025-04-28 21:52:03 -0300

    ✨ Adiciona função de subtração na calculadora

--- calculator.js
+++ calculator.js
@@ -2,7 +2,9 @@
 function sum(v1, v2) {}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {
+    return v1 - v2;
+  }
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
@@ -12,5 +14,6 @@ function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
+
 
 console.log(sum(v1, v2));
