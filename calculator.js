20c8d731d23dd9e62313c246ee854d103b894a3e
João Marcelo Bitar <joaomarcelo.bitar@gmail.com>
2025-04-29 07:19:33 -0300

    ✨Add Division Function

--- calculator.js
+++ calculator.js
@@ -13,7 +13,7 @@ function sub(v1, v2) {
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {
-    return v1/v3
+    return v1/v2
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
@@ -23,3 +23,5 @@ function mult(v1, v2) {}
 function square(v1) {}
 
 console.log(sum(v1, v2));
+console.log(sub(v1, v2));
+console.log(div(v1, v2));
